67de92b577f8aedd748cecb29a1f90c247c280d4
Joshwa12910 <64808405+Joshwa12910@users.noreply.github.com>
2023-11-16 11:35:32 -0500

    full test case

--- testCase.py
+++ testCase.py
@@ -24,5 +24,45 @@ class TestCourseSchedulingSystem(unittest.TestCase):
         expected_output = "\nYour Schedule\nCourse: CS120 Section: 001 Days: MWF Time: 0900 - 0950"
         self.assertIn(expected_output, mock_stdout.getvalue())
 
+    @patch("builtins.input", side_effect=["3", "CS120", "CS121", "CS222"])
+    def test_multiple_course_scheduling(self, mock_input):
+        file_path = "file.txt"
+
+        # Redirect stdout to capture print statements
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            courses = read_course_data(file_path)
+            display_courses(courses)
+
+            n = number_of_courses()
+            selected_courses = enter_course_numbers(courses, n)
+            self.assertEqual(selected_courses, {"CS120", "CS121", "CS222"})
+
+            schedule = generate_schedule(courses, selected_courses)
+            display_schedule(schedule)
+
+        # Check the printed output
+        expected_output = "\nYour Schedule\nCourse: CS120 Section: 001 Days: MWF Time: 0900 - 0950\nCourse: CS121 Section: 001 Days: TR Time: 1230 - 1345\nCourse: CS222 Section: 005 Days: TR Time: 1100 - 1215"
+        self.assertIn(expected_output, mock_stdout.getvalue())
+
+    @patch("builtins.input", side_effect=["2", "CS120", "CS121"])
+    def test_conflicting_course_scheduling(self, mock_input):
+        file_path = "file.txt"
+
+        # Redirect stdout to capture print statements
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            courses = read_course_data(file_path)
+            display_courses(courses)
+
+            n = number_of_courses()
+            selected_courses = enter_course_numbers(courses, n)
+            self.assertEqual(selected_courses, {"CS120", "CS121"})
+
+            schedule = generate_schedule(courses, selected_courses)
+            display_schedule(schedule)
+
+        # Check the printed output
+        expected_output = "Debugging: Time conflict for course"
+        self.assertIn(expected_output, mock_stdout.getvalue())
+
 if __name__ == "__main__":
     unittest.main()
